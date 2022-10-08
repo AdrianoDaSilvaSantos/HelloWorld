d7709adb4563ae84c28355cf483b8a760b370be3
AdrianoDaSilvaSantos <contatoadriano87@gmail.com>
2022-10-08 17:20:11 -0300

    Add LocalDate

--- src/br/com/hello/Main.java
+++ src/br/com/hello/Main.java
@@ -1,7 +1,11 @@
 package br.com.hello;
 
+import java.time.LocalDate;
+
 public class Main {
     public static  void main(String[] args) {
         System.out.println("Hello world!");
+        LocalDate ld = LocalDate.now();
+        System.out.println(ld);
     }
 }
